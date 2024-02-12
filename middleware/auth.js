741a1a55b4a6148612f664cc1639677eebb7d5ae
leslie pacheco <lesli_sharai_pacheco@hotmail.com>
2024-02-12 14:58:28 -0600

    creación de middlewers fixes #4

--- middleware/auth.js
+++ middleware/auth.js
@@ -18,19 +18,42 @@ module.exports = (secret) => (req, resp, next) => {
       return next(403);
     }
 
+    req.userId = decodedToken.id;
+    req.userRole = decodedToken.role;
+    console.log(req.userId, req.userRole);
+    return next();
+
     // TODO: Verify user identity using `decodeToken.uid`
   });
 };
 
-module.exports.isAuthenticated = (req) => (
-  // TODO: Decide based on the request information whether the user is authenticated
-  false
-);
+module.exports.isAuthenticated = (req) => {
 
-module.exports.isAdmin = (req) => (
+  const userId = req.userId ? req.userId.toString() : null;
+  if(userId){
+    console.log("autenticado",userId);
+    return true;
+  }else{
+    console.log(" no autenticado");
+    return false;
+  }
+  // TODO: Decide based on the request information whether the user is authenticated
+};
+
+module.exports.isAdmin = (req) => {
+
+  const userRol = req.userRole ? req.userRole.toString(): null;
+
+  if(userRol === "admin" || userRol === "Admin"){
+    console.log("Es admin");
+    // TODO: Decide based on the request information whether the user is an admin
+    return true;
+  }else{
+    console.log("No es admin");
+    return false;
+  }
   // TODO: Decide based on the request information whether the user is an admin
-  false
-);
+};
 
 module.exports.requireAuth = (req, resp, next) => (
   (!module.exports.isAuthenticated(req))
